381c74599ac8dadf9a43ee73a51f9372f85baaa9
azranel <chunkyo@gmail.com>
2017-08-10 14:25:18 +0200

    Add user to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810093135) do
+ActiveRecord::Schema.define(version: 20170810101532) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170810093135) do
     t.text "description"
     t.datetime "completed_at"
     t.datetime "due_to"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 20170810093135) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tasks", "users"
 end
