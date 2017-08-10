e864fe8994109603ad04499b6f896e9ec6dd2a02
azranel <chunkyo@gmail.com>
2017-08-10 14:27:15 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+if User.count >= 1000
+  puts "Creating users..."
+  1000.times do
+    User.create(
+      name: FFaker::Name.name,
+      email: FFaker::Internet.email
+    )
+    print '.'
+  end
+  puts "1000 Users created!"
+end
+
+
+puts "Creating tasks..."
+users = User.all
+1000.times do
+  name_length = rand(5) + 1
+  due_to = rand(1).zero? ? nil : rand(80).days.from_now
+  Task.create(
+    name: FFaker::Lorem.words(name_length).join(" "),
+    description: FFaker::Lorem.sentence,
+    due_to: due_to,
+    user: users.sample
+  )
+  print "."
+end
+
+puts "1000 tasks created"
