12889bba7ca7b2bb09bfd61c3c65a8dfe06f144f
azranel <chunkyo@gmail.com>
2017-08-10 11:36:41 +0200

    Create table Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810074430) do
+ActiveRecord::Schema.define(version: 20170810093135) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20170810074430) do
     t.text "description"
     t.datetime "completed_at"
     t.datetime "due_to"
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
